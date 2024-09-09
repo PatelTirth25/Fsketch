70c0d6e2f3a28e8fcfd45aff2877edd3a0529d16
Tirth Patel <tirthpatel204@gmail.com>
2024-09-09 16:25:37 +0530

    Dockerfile added

--- server/socket.js
+++ server/socket.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -28,7 +28,6 @@ io.on("connection", (socket) => {
   })
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
   });
 
 })
